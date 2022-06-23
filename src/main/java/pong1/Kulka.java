65d3c8660fb7e2c54d528a418cca7e5c8d589e30
Maciej Kosakowski <kosakowski222@gmail.com>
2022-06-23 14:37:21 +0200

    Punkt 11, Rozszerzona została funkcjonalność klasy Rugby. Wprowadzone zostały atrybuty omega oraz rot.

--- src/main/java/pong1/Kulka.java
+++ src/main/java/pong1/Kulka.java
@@ -2,6 +2,8 @@ package pong1;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
 
 public class Kulka {
     private static final double R = 10;
@@ -39,22 +41,29 @@ public class Kulka {
 
      @Override
      public void draw(GraphicsContext gc){
+         gc.save();
          gc.setFill(color);
+         gc.transform(new Affine(new Rotate(rot, xPos, yPos)));
          gc.fillOval(xPos - rx, yPos - ry, 2*rx, 2*ry);
          gc.restore();
      }
 
+     @Override
      public void update(){
-         xPos += xSpeed;
-         yPos += ySpeed;
+         super.update();
+         rot += omega;
+         System.out.printf("rotation: %f\n", rot);
      }
 
-    private double rx;
-    private double ry;
+    protected double rx;
+    protected double ry;
+    protected double omega;
+    protected double rot;
 
-    public Rugby(double xPos, double yPos, double xSpeed, double ySpeed, double rx, double ry) {
+    public Rugby(double xPos, double yPos, double xSpeed, double ySpeed, double omega, double rx, double ry) {
         super(xPos, yPos, xSpeed, ySpeed);
         this.rx = rx;
         this.ry = ry;
+        this.omega=omega;
     }
 }
